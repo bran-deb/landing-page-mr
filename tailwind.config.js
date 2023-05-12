b1a0c5fe15596de31ce18db1fec89f76e56cafe2
Brand-db <jairo.padilla.deb19@gmail.com>
2023-05-11 23:44:34 -0400

    Add(page):  config page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'wave-pattern': 'url(../assets/img/wave-white.png)',
+        'wave-pattern-dots': "url('../assets/img/pattern-white-dots.png')"
       },
     },
   },
