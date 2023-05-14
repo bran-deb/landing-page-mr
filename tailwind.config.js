fe9ad23f826d99b1385f15bb882dd3c7e5b6aaba
Brand-db <jairo.padilla.deb19@gmail.com>
2023-05-13 23:37:55 -0400

    add: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,17 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'wave-pattern': 'url(../assets/img/wave-white.png)',
-        'wave-pattern-dots': "url('../assets/img/pattern-white-dots.png')"
+        'wave-pattern-dots': "url('../assets/img/pattern-white-dots.png')",
+        'pattern-characters-blue': "url('../assets/img/pattern-characters-blue.png')",
+        'character-mario': "url('../assets/img/character-l.svg')",
+        'wave-pink': "url('../assets/img/wave-pink.png')",
+        'yellow-dots': "url('../assets/img/pattern-yellow-dots.png')",
       },
+      screens: {
+        'xsm' : '320px',
+      }
     },
   },
   plugins: [],
