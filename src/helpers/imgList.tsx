6816734eef920de6f822899e2b0f88049bbb6b93
Brand-db <jairo.padilla.deb19@gmail.com>
2023-05-15 19:32:34 -0400

    update: styles tailwind

---
+++ src/helpers/imgList.tsx
@@ -0,0 +1,6 @@
+export { default as imgCard1 } from 'src/assets/img/abstract.jpg'
+export { default as imgCard2 } from 'src/assets/img/abstract3.jpg'
+export { default as imgcard3 } from 'src/assets/img/abstract4.jpg'
+export { default as imgHome } from 'src/assets/img/abstract2.jpg'
+export { default as imgHome2 } from 'src/assets/img/abstract22.jpg'
+export { default as imgCat } from 'src/assets/img/cat.gif'
